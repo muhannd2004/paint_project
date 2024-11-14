f670348f87a51f570b37ba884c559ddeef5a6452
muhannd2004 <muhanndsayed24@gmail.com>
2024-11-14 19:56:11 +0200

    test2

---
+++ paintBackend/src/test/java/paint/example/paintBackend/PaintBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package paint.example.paintBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaintBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
